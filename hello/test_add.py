fd5a47c33741a5aa00340f0a42bb370dd6405671
Warren MacEvoy <wmacevoy@gmail.com>
2023-09-28 03:57:19 -0600

    working add

--- hello/test_add.py
+++ hello/test_add.py
@@ -1,12 +1,27 @@
-import unittest
+from pytest import approx
+from add import add
 
-import add
+def test_add_ints():
+    assert add(1,2) == 3
+    assert add(1,-1) == 0
 
+    # Python can represent large integers exactly if they fit in memory.
+    # Many languages use a fixed-size representations which leads to
+    # overflow (but runs much faster, which is the trade-off).
+    #
+    # Do not expect this to be correct in C or C++!
+    #
+    assert add(10**40,1) == 10000000000000000000000000000000000000001
 
-class TestAdd(unittest.TestCase):
-    def test_add_int(self):
-        self.assertEqual(add.add(1, 2), 3)
-        self.assertEqual(add.add(1, -1), 0)
+def test_add_floats():
+    # Python approximates fractions/floats in base 2 exponential
+    # notation with 54 digits in the mantessa and a power of 2 exponent
+    #  x ≈ ±1.011...(50 more binary digits) x 2^exponent
+    # so floating point calculations are close but usually not exact.
+    
+    # 0.8333333333333334 != 0.8333333333333333
+    assert 1-1/6 != add(1/3,1/2)
 
-    def test_add_float(self):
-        self.assertEqual(add.add(1.0, 2.0), 3.0)
+    # But it is close (approx can specify a relative or absolute error).
+    # https://docs.pytest.org/en/7.1.x/reference/reference.html#pytest-approx
+    assert approx(1-1/6,rel=1e-12) == add(1/3,1/2)
