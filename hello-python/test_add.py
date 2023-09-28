29291dd50557ace2dec738c46ceb418cf95db043
Ram Basnet <rambasnet@gmail.com>
2023-09-27 22:00:39 -0600

    add utility

--- hello-python/test_add.py
+++ hello-python/test_add.py
@@ -4,6 +4,9 @@ import add
 
 
 class TestAdd(unittest.TestCase):
-    def test_add(self):
+    def test_add_int(self):
         self.assertEqual(add.add(1, 2), 3)
         self.assertEqual(add.add(1, -1), 0)
+
+    def test_add_float(self):
+        self.assertEqual(add.add(1.0, 2.0), 3.0)
